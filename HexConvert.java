c885a4c483b02b1c300441c286c24d988775fd84
JayMar <72720429+jaymar921@users.noreply.github.com>
2021-10-19 08:10:39 -0400

    Add files via upload

---
+++ HexConvert.java
@@ -0,0 +1,25 @@
+public class HexConvert {
+
+    public static String toHex(String str){
+        StringBuilder sb = new StringBuilder();
+        //Converting string to character array
+        char[] ch = str.toCharArray();
+        for (char c : ch) {
+            String hexString = Integer.toHexString(c);
+            sb.append(hexString);
+        }
+        return sb.toString();
+    }
+
+    public static String fromHex(String str){
+        StringBuilder result = new StringBuilder();
+        char[] charArray = str.toCharArray();
+        for(int i = 0; i < charArray.length; i=i+2) {
+            String st = ""+charArray[i]+""+charArray[i+1];
+            char ch = (char)Integer.parseInt(st, 16);
+            result.append(ch);
+        }
+        return result.toString();
+
+    }
+}
